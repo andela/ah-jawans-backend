b8f3c9b6b76b5ed84def8e9aec47fba4792e5aec
Michael Nzube <michaelnzube@Michaels-MacBook-Pro.local>
2019-06-26 11:17:07 +0200

    ch(swagger): implementing swagger configuration
    - added file swagger.json with user creation route
    - Added swagger express package
    - added swagger routes on index.js on route folder

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
-const router = require("express").Router();
+const express=require('express');
+const apiRouter = require('./api');
+const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+ 
 
-router.use("/api", require("./api"));
+router.use("/api", apiRouter);
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
+
