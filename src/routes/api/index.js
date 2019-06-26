13fdb499c5a04e325fd8511f518bf2b720dd5c8a
shalu <chandwani.shalu@andela.com>
2019-06-26 16:53:35 +0200

    ch(es6): converting to es6
    - Removed unwanted code
    - changed code to es6 format
    
    [(Finishes #166901840]
    
    Update code base from es5 to es6

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,18 +1,9 @@
-const router = require("express").Router();
+import express from 'express';
 
-router.use("/", require("./users"));
+import users from './users';
 
-router.use(function(err, req, res, next) {
-    if (err.name === "ValidationError") {
-        return res.status(422).json({
-            errors: Object.keys(err.errors).reduce(function(errors, key) {
-                errors[key] = err.errors[key].message;
-                return errors;
-            }, {})
-        });
-    }
+const router = express.Router();
 
-    return next(err);
-});
+router.use('/', users);
 
-module.exports = router;
+export default router;
