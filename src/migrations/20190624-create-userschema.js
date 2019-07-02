0e1ec9d7216633a32b6f8d99b45ff3081d2abe7d
Joseph Nkurunziza <josetheblessing@gmail.com>
2019-07-02 17:56:44 +0200

    ft(Generate Token):Allow user to receive JWT on successful registration/login
    - Install depandencies needed
    - Hash password
    - Edit table schema
    - Replaced MongoDB code with Sequelize
    - Allow user to signup
    - Allow user to signin
    - Generate and send JWT Token in the response
    - Add unit tests for functions implemented
    
    [(Finishes #166790084)]

--- src/migrations/20190624-create-userschema.js
+++ src/migrations/20190624-create-userschema.js
@@ -1,50 +1,62 @@
-'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('user', {
-        username: {
-        allowNull: false,
-        autoIncrement: false,
-        primaryKey: true,
-        type: Sequelize.STRING
-      },
-      mail: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        primaryKey: true
-      },
-      bio: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      image: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      favorites: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      following: {
-        type: Sequelize.STRING,
-        allowNull: false
 
-      },
-      hash: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      salt: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      timestamps: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('user');
-  }
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    firstName: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+    lastName: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+    username: {
+      allowNull: false,
+      autoIncrement: false,
+      unique: true,
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+    bio: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    image: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    following: {
+      type: Sequelize.STRING,
+      allowNull: true
+
+    },
+    timestamps: {
+      allowNull: true,
+      type: Sequelize.DATE
+    },
+    createdAt: {
+      allowNull: true,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: true,
+      type: Sequelize.DATE
+    }
+  }),
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
 };
