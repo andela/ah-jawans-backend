58be1a9628163175ae9449d2be8db0b37b855300
kagabof <faustinkagabo@gmail.com>
2019-07-03 13:26:23 +0200

    ft(validation errors): signup/Registration validation errors
    - added schema of validating in helpers/validation.js
    - Added bodyvalidation that generate erros middleware/bodyValidation.js
    - Added signupvalidation middeware/signupvalidation
    
    [( #166790086]

---
+++ src/middlewares/signupValidation.js
@@ -0,0 +1,32 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+import models from '../models';
+
+class ValidateUser {
+  static async validateUser(req, res, next) {
+    const { email } = req.body;
+    const { User } = models;
+    try {
+      await User.findAll({
+        where: {
+          email: req.body.email
+        }
+      })
+        .then((user) => {
+          if (user.length) {
+            return res.status(409).json({
+              message: `Email ${email} exists in the system!`
+            });
+          }
+          next();
+        });
+    } catch (error) {
+      return res.status(500).json({
+        error: 'Internal Server Error!'
+      });
+    }
+  }
+}
+
+
+export default ValidateUser;
