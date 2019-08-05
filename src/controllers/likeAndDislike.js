a98b89691e8fb65ebc07cad46560de719b8d4eaf
Patrick Ngabonziza <ngabonzizap7@gmail.com>
2019-08-06 01:22:45 +0200

    bg(likes): refactoring likes and dislikes

--- src/controllers/likeAndDislike.js
+++ src/controllers/likeAndDislike.js
@@ -4,60 +4,69 @@ import findArticle from './helpers/findArticle';
 
 const { LikeAndDislike } = model;
 
+const constDeaclarations = async (req) => {
+  const articleId = req.params.id;
+  const { id } = req.user;
+  const article = await findArticle(articleId);
+  return { article, id };
+};
+
 /**
   * This class contains Like and dislike controller
   */
 class LikesAndDislikes {
-/**
- *
- * @param {Object} req
- * @param {Object} res
- * @returns {Object} Number of likes
- */
+  /**
+   *
+   * @param {Object} req
+   * @param {Object} res
+   * @returns {Object} Number of likes
+   */
   static async likeArticle(req, res) {
-    const articleId = req.params.id;
-    const { id } = req.user;
-    const article = await findArticle(articleId);
+    try {
+      const articleconst = await constDeaclarations(req);
+      if (!articleconst.article) return res.status(404).json({ status: 404, message: 'Article not found' });
 
-    if (!article) return res.status(404).json({ status: 404, message: 'Article not found' });
+      const userReacted = await
+      LikesDislikesHelpers.userLikedOrDiskedArticle(req.user.id, articleconst.article.id);
+      if (!userReacted[0]) {
+        await LikeAndDislike.create({ userId: req.user.id,
+          articleId: articleconst.article.id,
+          likes: true,
+          dislikes: false });
+        const likes = await
+        LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId: articleconst.article.id,
+          likes: true } });
+        return res.status(200).json({ status: 200, likes });
+      }
 
-    const liked = await LikesDislikesHelpers.findArticleLikes(id, articleId);
-    const disliked = await LikesDislikesHelpers.findDisliked(id, articleId);
-    const unlikedArticle = await LikesDislikesHelpers.findUnlikedArticle(id,
-      articleId, { dislikes: false }, { likes: true });
-    const userReacted = await LikesDislikesHelpers.userLikedOrDiskedArticle(id, articleId);
+      if (userReacted[0].likes === true && userReacted[0].dislikes === false) {
+        await LikesDislikesHelpers.ChangeLikeStatus({ dislikes: false, likes: false },
+          userReacted[0].id);
+        const likes = await
+        LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId: articleconst.article.id,
+          likes: true } });
+        return res.status(200).json({ status: 200, likes });
+      }
 
-    if (!userReacted[0]) {
-      await LikeAndDislike.create({ userId: id,
-        articleId: article.id,
-        likes: true,
-        dislikes: false });
-      const likes = await LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId,
-        likes: true } });
-      return res.status(200).json({ status: 200, likes });
-    }
+      if (userReacted[0].likes === false && userReacted[0].dislikes === false) {
+        await LikesDislikesHelpers.ChangeLikeStatus({ dislikes: false, likes: true },
+          userReacted[0].id);
+        const likes = await
+        LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId: articleconst.article.id,
+          likes: true } });
+        return res.status(200).json({ status: 200, likes });
+      }
 
-    if (liked[0]) {
-      await LikesDislikesHelpers.updateArticlesLiskes(liked[0].id);
-      const likes = await LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId,
-        likes: true } });
-      return res.status(200).json({ status: 200, likes });
-    }
-
-    if (unlikedArticle[0]) {
-      await LikesDislikesHelpers.ChangeLikeStatus({ dislikes: false, likes: true },
-        unlikedArticle[0].id);
-      const likes = await LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId,
-        likes: true } });
-      return res.status(200).json({ status: 200, likes });
-    }
-
-    if (disliked[0]) {
-      await LikesDislikesHelpers.ChangeLikeStatus({ dislikes: false, likes: true },
-        disliked[0].id);
-      const likes = await LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId,
-        likes: true } });
-      return res.status(200).json({ status: 200, likes });
+      if (userReacted[0].likes === false && userReacted[0].dislikes === true) {
+        await LikesDislikesHelpers.ChangeLikeStatus({ dislikes: false, likes: true },
+          userReacted[0].id);
+        const likes = await
+        LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId: articleconst.article.id,
+          likes: true } });
+        return res.status(200).json({ status: 200, likes });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: 'Server error' });
     }
   }
 
@@ -67,48 +76,50 @@ class LikesAndDislikes {
     * @return {object} Number of Dislikes
     */
   static async dislikeArticle(req, res) {
-    const articleId = req.params.id;
-    const { id } = req.user;
-    const article = await findArticle(articleId);
+    try {
+      const articleconst = await constDeaclarations(req);
+      if (!articleconst.article) return res.status(404).json({ status: 404, message: 'Article not found' });
 
-    if (!article) return res.status(404).json({ status: 404, message: 'Article not found' });
+      const userReacted = await
+      LikesDislikesHelpers.userLikedOrDiskedArticle(req.user.id, articleconst.article.id);
+      if (!userReacted[0]) {
+        await LikeAndDislike.create({ userId: req.user.id,
+          articleId: articleconst.article.id,
+          likes: false,
+          dislikes: true });
+        const dislikes = await
+        LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId: articleconst.article.id,
+          dislikes: true } });
+        return res.status(200).json({ status: 200, dislikes });
+      }
 
-    const dislikedArticle = await LikesDislikesHelpers.findUnlikedArticle(id,
-      articleId, { dislikes: true }, { likes: false });
-    const disliked = await LikesDislikesHelpers.findDisliked(id, articleId);
-    const liked = await LikesDislikesHelpers.findArticleLikes(id, articleId);
-    const userReacted = await LikesDislikesHelpers.userLikedOrDiskedArticle(id, articleId);
+      if (userReacted[0].likes === false && userReacted[0].dislikes === true) {
+        await LikesDislikesHelpers.ChangeLikeStatus({ dislikes: false, likes: false },
+          userReacted[0].id);
+        const dislikes = await
+        LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId: articleconst.article.id,
+          dislikes: true } });
+        return res.status(200).json({ status: 200, dislikes });
+      }
 
-    if (!userReacted[0]) {
-      await LikeAndDislike.create({ userId: id,
-        articleId: article.id,
-        dislikes: true,
-        likes: true });
-      const dislikes = await LikesDislikesHelpers.countArticleDisikesLikes(article.dataValues.id,
-        { dislikes: true });
-      return res.status(200).json({ status: 200, dislikes });
-    }
-
-    if (disliked[0]) {
-      await LikesDislikesHelpers.updateArticleDisliskes(disliked[0].id);
-      const dislikes = await LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId,
-        dislikes: true } });
-      return res.status(200).json({ status: 200, dislikes });
-    }
-    if (dislikedArticle[0]) {
-      await LikesDislikesHelpers.ChangeLikeStatus({ dislikes: true, likes: false },
-        dislikedArticle[0].id);
-      const dislikes = await LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId,
-        dislikes: true } });
-      return res.status(200).json({ status: 200, dislikes });
-    }
-
-    if (liked[0]) {
-      await LikesDislikesHelpers.ChangeLikeStatus({ dislikes: true, likes: false },
-        liked[0].id);
-      const dislikes = await LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId,
-        dislikes: true } });
-      return res.status(200).json({ status: 200, dislikes });
+      if (userReacted[0].likes === false && userReacted[0].dislikes === false) {
+        await LikesDislikesHelpers.ChangeLikeStatus({ dislikes: true, likes: false },
+          userReacted[0].id);
+        const dislikes = await
+        LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId: articleconst.article.id,
+          dislikes: true } });
+        return res.status(200).json({ status: 200, dislikes });
+      }
+      if (userReacted[0].likes === true && userReacted[0].dislikes === false) {
+        await LikesDislikesHelpers.ChangeLikeStatus({ dislikes: true, likes: false },
+          userReacted[0].id);
+        const dislikes = await
+        LikesDislikesHelpers.countArticleDisikesLikes({ where: { articleId: articleconst.article.id,
+          dislikes: true } });
+        return res.status(200).json({ status: 200, dislikes });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: 'Server error' });
     }
   }
 }
